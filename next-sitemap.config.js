72e03837a274e942614f5cbd076dc8f43f50f3e9
Arbaj-09 <arbajshaikh9561@gmail.com>
2025-06-04 17:48:48 +0530

    update

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/**
+ * @type {import('next-sitemap').IConfig}
+ */
+module.exports = {
+  siteUrl: 'https://www.worldtriplink.com', // TODO: Update to your actual domain
+  generateRobotsTxt: true, // (optional)
+  // ...other options can be added here
+};
